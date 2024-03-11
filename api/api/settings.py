4358be3df2336f0ad9859a943067e7d5265cd15a
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-03-11 15:47:47 -0300

    chore: add rest_framework and posts app in installed_apps and add rest_framework config

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
+
+    # Local
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
